574731bede68d51f48d35bcde3b3757ca9f33cbc
LifeIsMadness <Luckstriker073@gmail.com>
2020-06-13 19:54:39 +0300

    Bot moved to the class. News parser is ready

--- BotProject/bot.py
+++ BotProject/bot.py
@@ -4,15 +4,22 @@ import botutils
 from discord.ext import commands
 import logging
 import configparser
+import newsparser
 
 # setup logging
 logger = logging.getLogger('__name__')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 # read config
+
+
 def read_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -20,55 +27,152 @@ def read_config():
 
 
 TOKEN = read_config()
-bot = commands.Bot(command_prefix='!')
+news_parser = newsparser.IgromaniaNewsParser('key.txt')
+# bot = commands.Bot(command_prefix='!')
 
 
-@bot.command()
-async def hello(ctx):
-    is_owner = await bot.is_owner(ctx.message.author)
-    if is_owner:
-        await ctx.send('Здарова!')
-    else:
-        await ctx.send('Не борзей...')
+class BotClient(commands.Bot):
+    def register_commands(self):
+        self.add_command(self.hello)
+        self.add_command(self.say)
+        self.add_command(self.shutdown)
+        self.add_command(self.flip)
+        self.add_command(self.news)
 
 
-@bot.command()
-async def say(ctx, *args):
-    members = ctx.message.guild.members
-    member = None
-    for m in members:
-        if m.name == args[0]:
-            member = m
+    @commands.command()
+    async def hello(ctx):
+        is_owner = await bot.is_owner(ctx.message.author)
+        if is_owner:
+            await ctx.send('Здарова!')
+        else:
+            await ctx.send('Не борзей...')
+
+
+    @commands.command()
+    async def say(ctx, *args):
+        members = ctx.message.guild.members
+        member = None
+        for m in members:
+            if m.name == args[0]:
+                member = m
             logger.debug(f'{member} and {member.name}')
             break
-    if member is None:
-        for m in members:
-            if m.nick == args[0]:
-                member = m
-                logger.debug(f'{member} and {member.nick}')
-                break
-    _, *phrase = args
-    if member:
-        await ctx.send('{} {}'.format(member.mention, ' '.join(args)))
-    else:
-        await ctx.send(f'Данный pidor не найден')
+        if member is None:
+            for m in members:
+                if m.nick == args[0]:
+                    member = m
+                    logger.debug(f'{member} and {member.nick}')
+                    break
+        _, *phrase = args
+        if member:
+            await ctx.send('{} {}'.format(member.mention, ' '.join(args)))
+        else:
+            await ctx.send(f'Данный pidor не найден')
 
 
-@bot.command()
-@commands.is_owner()
-async def shutdown(ctx):
-    await ctx.bot.logout()
+    @commands.command()
+    @commands.is_owner()
+    async def shutdown(ctx):
+        await ctx.bot.logout()
 
 
-@bot.command()
-async def flip(ctx):
-    members = botutils.get_online_members(ctx.message.guild.members)
-    lucky_one = random.randint(0, len(members) - 1)
+    @commands.command()
+    async def flip(ctx):
+        members = botutils.get_online_members(ctx.message.guild.members)
+        lucky_one = random.randint(0, len(members) - 1)
 
-    await ctx.send('Выбираю лалку...')
-    await asyncio.sleep(5)
-    await ctx.send(f'Похоже {members[lucky_one].name} лалка)')
+        await ctx.send('Выбираю лалку...')
+        await asyncio.sleep(5)
+        await ctx.send(f'Похоже {members[lucky_one].name} лалка)')
 
 
+    @commands.command()
+    async def news(ctx):
+        is_owner = await bot.is_owner(ctx.message.author)
+        if is_owner:
+            bot.loop.create_task(task_news(ctx))
+        else:
+            await ctx.send('Нет доступа.')
+
+
+    async def task_news(ctx):
+        logger.debug('Here')
+        while True:
+            news = news_parser.parse_news()
+            logger.debug('Exploring news...')
+            for news_element in news:
+                await ctx.send(
+                    news_parser.site_url + news_element.a.get('href'))
+            await asyncio.sleep(1800)
+
+
+# @bot.command()
+# async def hello(ctx):
+#     is_owner = await bot.is_owner(ctx.message.author)
+#     if is_owner:
+#         await ctx.send('Здарова!')
+#     else:
+#         await ctx.send('Не борзей...')
+
+
+# @bot.command()
+# async def say(ctx, *args):
+#     members = ctx.message.guild.members
+#     member = None
+#     for m in members:
+#         if m.name == args[0]:
+#             member = m
+#             logger.debug(f'{member} and {member.name}')
+#             break
+#     if member is None:
+#         for m in members:
+#             if m.nick == args[0]:
+#                 member = m
+#                 logger.debug(f'{member} and {member.nick}')
+#                 break
+#     _, *phrase = args
+#     if member:
+#         await ctx.send('{} {}'.format(member.mention, ' '.join(args)))
+#     else:
+#         await ctx.send(f'Данный pidor не найден')
+
+
+# @bot.command()
+# @commands.is_owner()
+# async def shutdown(ctx):
+#     await ctx.bot.logout()
+
+
+# @bot.command()
+# async def flip(ctx):
+#     members = botutils.get_online_members(ctx.message.guild.members)
+#     lucky_one = random.randint(0, len(members) - 1)
+
+#     await ctx.send('Выбираю лалку...')
+#     await asyncio.sleep(5)
+#     await ctx.send(f'Похоже {members[lucky_one].name} лалка)')
+
+
+# @bot.command()
+# async def news(ctx):
+#     is_owner = await bot.is_owner(ctx.message.author)
+#     if is_owner:
+#         bot.loop.create_task(task_news(ctx))
+#     else:
+#         await ctx.send('Нет доступа.')
+
+
+# async def task_news(ctx):
+# 	logger.debug('Here')
+# 	while True:
+# 		news = news_parser.parse_news()
+
+# 		logger.debug(news)
+# 		for news_element in news:
+# 			await ctx.send(news_parser.site_url + news_element.a.get('href'))
+# 		await asyncio.sleep(1800)
 if __name__ == '__main__':
+    bot = BotClient(command_prefix='!')
+    bot.register_commands()
     bot.run(TOKEN)
--- BotProject/newsparser.py
+++ BotProject/newsparser.py
@@ -3,60 +3,50 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
-
-
 class IgromaniaNewsParser(object):
-#    news_url = 'https://www.igromania.ru/news/'
-#    html_text = requests.get(news_url).text
-#    soup = BeautifulSoup(html_text, 'html.parser')
-    
-    
     def __init__(self, last_news_file):
         self._last_news_file = last_news_file
-        self._read_last_key()
-        self._site_url = 'https://www.igromania.ru/'
-    
-    
-    
+        self.site_url = 'https://www.igromania.ru/'
+
     def _read_last_key(self):
         with open(self._last_news_file, 'r') as desc:
-            self._last_news_key = desc.read()
-    
-    
+            return desc.read().strip()
+
     def _store_last_key(self, last_news_key):
         with open(self._last_news_file, 'w') as desc:
-            desc.write(last_news_key)      
-    
-    
+            desc.write(last_news_key)
+
     def _create_soup(self):
-        html_text = requests.get(self._site_url + 'news/').text
+        html_text = requests.get(self.site_url + 'news/').text
         soup = BeautifulSoup(html_text, 'html.parser')
         return soup
-    
-    
-    def _news_updated(self):
+
+    def _news_updated(self, last_news_key):
         soup = self._create_soup()
         attrs = {
             'class': 'aubl_item',
-	        
+
         }
-        
-        news_element =  soup.find('div', attrs=attrs)
-        return news_element.a.img.get('src') + '\n' != self._last_news_key
-              
-    
+
+        news_element = soup.find('div', attrs=attrs)
+        return news_element.a.img.get('src') != last_news_key
+
     def parse_news(self):
-        if not self._news_updated():
-            return
+        last_news_key = self._read_last_key()
+        if not self._news_updated(last_news_key):
+            return []
         soup = self._create_soup()
         attrs = {
             'class': 'aubl_item',
-	        
+
         }
-        
-        news_elements =  soup.find_all('div', attrs=attrs, limit=3)
-        self._store_last_key(news_elements[0].a.img.get('alt'))
+
+        news_elements = soup.find_all('div', attrs=attrs, limit=3)
+        for i in range(1, 3):
+            if news_elements[i].a.img.get('src') + '\n' == last_news_key:
+                news_elements = news_elements[:i]
+                break
+        self._store_last_key(news_elements[0].a.img.get('src'))
         news_elements.reverse()
         return news_elements
 #        for element in news_elements:
@@ -66,4 +56,3 @@ class IgromaniaNewsParser(object):
 if __name__ == '__main__':
     news_parser = IgromaniaNewsParser('key.txt')
     news_parser.parse_news()
-
